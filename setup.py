204ccf897263fa6fac0145a5e7d7d4ed5aeff33e
Erik Kučák <riko98@azet.sk>
2019-11-19 22:08:50 +0100

    Initialized project

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='SAT solver analyzer',
+     version='0.1',
+     scripts=[],
+     author="Erik Kucak",
+     author_email="Riko196@gmail.com",
+     description="SAT solver analyzer",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/Riko196/SAT-solver-analyzer",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
