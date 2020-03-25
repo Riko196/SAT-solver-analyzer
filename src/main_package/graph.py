aeca3dee21fcee59796b15746767cb2d2f9a0b94
Erik Kučák <riko98@azet.sk>
2020-03-25 19:57:39 +0100

    Improved graph

--- src/main_package/graph.py
+++ src/main_package/graph.py
@@ -1,11 +1,22 @@
-from matplotlib import pyplot as plt
+from matplotlib import pyplot
+from pandas import DataFrame
 
 class Graph:
-    def __init__(self, iterations, times):
+    def __init__(self, iterations, averages, maximums, title):
         self.iterations = iterations
-        self.times = times
+        self.averages = averages
+        self.maximums = maximums
+        self.title = title
 
     def render(self):
-        plt.title("Evolving formulas computing time")
-        plt.plot(self.iterations, self.times)
-        plt.show()
+        data = DataFrame({'Iteration': self.iterations, 'Average': self.averages, 'Maximum': self.maximums})
+        pyplot.title(self.title)
+
+        pyplot.plot('Iteration', 'Average', data=data, color='blue')
+        pyplot.plot('Iteration', 'Maximum', data=data, color='black')
+
+        pyplot.xlabel('Iteration')
+        pyplot.ylabel('Time(s)')
+
+        pyplot.legend()
+        pyplot.show()
