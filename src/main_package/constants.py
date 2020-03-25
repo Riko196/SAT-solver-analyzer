c61b434c6e8f7cac5633f64460db734ae4a80a35
Erik Kučák <riko98@azet.sk>
2020-03-25 19:58:27 +0100

    Improved constants

--- src/main_package/constants.py
+++ src/main_package/constants.py
@@ -7,9 +7,11 @@ satSolverCommands = [
 
 def setConstants():
     environ['K'] = '3'
-    environ['POPULATION'] = '500'
-    environ['ITERATIONS'] = '300'
+    environ['POPULATION'] = '100'
+    environ['ITERATIONS'] = '10'
     environ['VARIABLES'] = '150'
+    environ['CLAUSES'] = str(int(int(getenv('VARIABLES')) * 4.2))
+    environ['INITIALIZATION_FORMULAS'] = '100'
     environ['FORMULA_FILE'] = 'formula.cnf'
     environ['MAXIMUM_FORMULA_FILE'] = 'maximum.cnf'
-    environ['SAT_SOLVER_CMD'] = satSolverCommands[0] + getenv('FORMULA_FILE') + ' > /dev/null'
+    environ['SAT_SOLVER_CMD'] = satSolverCommands[1] + getenv('FORMULA_FILE') + ' > /dev/null'
