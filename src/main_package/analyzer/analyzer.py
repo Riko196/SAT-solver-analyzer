6f6aefd4bedd26a318f0c52604dee3e510bdb297
Erik Kučák <riko98@azet.sk>
2020-03-25 19:57:30 +0100

    Added analyzer

---
+++ src/main_package/analyzer/analyzer.py
@@ -0,0 +1,16 @@
+from random import randint
+from os import getenv
+
+from ..formula.clause import Clause
+from ..formula.formula import Formula
+from ..generators.random_generator import RandomGenerator
+
+class Analyzer:
+    def determineCountOfVariables(self):
+        pass
+
+    def determineHeuristic(self):
+        pass
+
+    def analyzeEvolvedFormulas(self):
+        pass
